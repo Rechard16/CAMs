8ea4d6559193044c3155e016f9ed2eb7c890bb76
Bateauxplanze <82026272+Paraworks@users.noreply.github.com>
2023-10-30 15:12:50 +0800

    Delete src/Staff.java

--- src/Staff.java
+++
@@ -1,9 +0,0 @@
-public class Staff extends User{
-    private void createCamp(){}
-    private void editCamp(){}
-    private void deleteCamp(){}
-    private void toggleVisibility(){}
-    private void viewSession(){}
-    private void approveSession(){}
-    private void generateReport(){}
-}
