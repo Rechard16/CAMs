209187cdb133b6e04ffacaea4a4abdd0f1fae585
sheeshzr <131577433+sheeshzr@users.noreply.github.com>
2023-11-08 22:04:29 +0800

    Update UserManager.java

--- src/manager/UserManager.java
+++ src/manager/UserManager.java
@@ -1,51 +1,124 @@
 package manager;
 
-import model.User;
-import model.Student;
-import model.Staff;
+
+import java.util.ArrayList;
 import java.util.List;
+import model.Camp;
+import model.CampInfo;
+import java.util.Date;
 
-public class UserManager {
-    private List<Staff> staffs;
-    private List<Student> students;
 
-    public boolean addUser(User user) {
-        // Implement the method
+import model.Student;
+
+
+public class CampManager {
+	
+	private List<Camp> camps;
+	private int totalCamps;
+	
+	public CampManager() {
+	    camps = new ArrayList<Camp>();
+	}
+	
+
+    
+    public boolean createCamp(CampInfo campInfo) { //does not check if campInfo contains valid information
+    	//Initializes a temporary Camp(campInfo) object, appends it to the end of the ArrayList camps 
+    	
+    	if (campInfo != null) {
+    		Camp tempCamp = new Camp(campInfo);
+    		camps.add(tempCamp);
+    		totalCamps++;
+    		return true; //returns true if successful
+    	}
+    	else {
+    		return false; //returns false if campInfo is null
+    	}
+        
+    }
+
+    public boolean deleteCamp(int deleteId) {
+        //Iterating through the ArrayList of camp objects 
+    	//Finds the index of the camp with the deleteId, calls .remove(index) to remove it
+    	
+    	int i = 0;
+    	while (i < camps.size()) {
+    		if (deleteId == camps.get(i).getId()) {
+    			camps.remove(i);
+    			return true;
+    		}
+    		i++;
+    	}
+        return false; //returns false when unable to find the camp to be deleted
+    }
+
+    public boolean editCamp(int campID, CampInfo campInfo) {
+        //Iterates through the ArrayList to find index of the campID to be edited
+    	//Uses the .set() method to replace with the new campInfo
+    	
+    	int i = 0;
+    	while (i < camps.size()) {
+    		if (campID == camps.get(i).getId()) {
+    			Camp tempCamp = new Camp(campInfo);
+    			camps.set(i, tempCamp);
+    			return true; //returns true if successful
+    		}
+    		i++;
+    	}
+    	
+        return false; //returns false when unable to find camp with the inputed campID
+    }
+
+    //Should this method be in student class?
+    //Unable to access the private List<Integer> camps
+    //Alternatively, we could do an occupiedDates variable for the Student class, compare against the dates of camp under camp info (might need another method?)
+    public boolean checkClash(Student student, Camp camp) {
+    	//Checks whether the student is able to go for the camp
+    	return false; 
+    }
+    
+    //Not sure what this method is supposed to do
+    public boolean updateSlot(int campID, int number) {
+        
         return false;
     }
 
-    public boolean removeUser(String userID) {
-        // Implement the method
-        return false;
+    
+    public void toggleVisibility(int campID, boolean boolValue) {
+        
+    	//In order to add this method, I added another method,setVisibility() into the Camp class
+    	//This method might not be reflected in the other branches (Added here just in case)
+        /*
+         public void setVisbility(boolean visible) {
+        	if (visible == true) {
+        		this.visibility = true;
+        	}
+        	else {
+        		this.visibility = false;
+        	}
+        }
+         */
+    	
+    	int i = 0;
+    	while (i < camps.size()) {
+    		if (campID == camps.get(i).getId()) {
+    			camps.get(i).setVisibility(boolValue);
+    		}
+    		i++;
+    	}
+    	
+    	return;
     }
 
-    public boolean editUser(User user) {
-        // Implement the method
-        return false;
-    }
-
-    public User getUserByID(String userID) {
-        // Implement the method
-        return null;
-    }
-
-    public boolean checkCommittee(String studentID) {
-        // Implement the method
-        return false;
-    }
-
-    public boolean checkStaff(String staffID) {
-        // Implement the method
-        return false;
-    }
-
-    public boolean givePoint(Student student, int points) {
-        // Implement the method
-        return false;
-    }
-
-    public boolean loginUser(String userID, String password) {
-        // Implement the method
-        return false;
+    public Camp getCampById(int campID) {
+        
+    	int i = 0;
+    	while (i < camps.size()) {
+    		if (campID == camps.get(i).getId()) {
+    			return camps.get(i);
+    		}
+    	}
+    	
+        return null; //Returns null if unable to find
     }
 }
