4f80567dcffa0d5402e99a94e7c71b3f10c64b7d
pimangnono <pimangnono@gmail.com>
2023-11-10 18:41:54 +0800

    implement 'add' method

--- src/com/cams/manager/SuggestionManager.java
+++ src/com/cams/manager/SuggestionManager.java
@@ -4,13 +4,28 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import com.cams.model.Suggestion;
-import java.util.List;
-import database.SuggestionDatabase;
-import 
+import com.cams.model.Camp;
 
-public class SuggestionManager {
+import database.SuggestionDatabase;
+import model.Suggestion;
+
+public class SuggestionManager extends SuggestionDatabase  {
     private SuggestionDatabase suggestionDatabase;
 
-    public boolean acceptChanges(Camp)
+    //constructor
+    public SuggestionManager(){
+        this.suggestionDatabase = new SuggestionDatabase();
+    }
+
+    // add suggestion
+    public boolean addSuggestion(model.Suggestion suggestion){
+        if (suggestion != null)
+        {
+            model.Suggestion temp = new Suggestion(suggestion);
+            suggestionDatabase.add(temp);
+            return true;
+        }
+        return false;
+    }
 
 }
