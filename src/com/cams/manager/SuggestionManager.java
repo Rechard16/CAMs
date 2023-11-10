3da172c3f0dfda82ace9a4a817bcf3f98664388b
pimangnono <pimangnono@gmail.com>
2023-11-11 01:15:14 +0800

    implemented 'deleteSuggestion'

--- src/com/cams/manager/SuggestionManager.java
+++ src/com/cams/manager/SuggestionManager.java
@@ -14,30 +14,63 @@ import model.Camp;
 public class SuggestionManager extends SuggestionDatabase {
     private SuggestionDatabase suggestionDatabase;
  
-
+    // linking with suggestion database 
     public SuggestionManager() throws ClassNotFoundException, IOException {
         suggestionDatabase = new database.SuggestionDatabase();
         
     }
 
-    // Add suggestion
     public boolean addSuggestion(Camp camp, Suggestion suggestion, User user) throws IOException, ClassNotFoundException {
-        if ( suggestion != null) {
+        if (suggestion != null) {
+
             // Create a new Suggestion
-            Suggestion newSuggestion = new Suggestion(user, 98, suggestion.getDescription(), suggestionDatabase);
+            Suggestion newSuggestion = new Suggestion(user, camp.getId(), suggestion.getDescription(), suggestionDatabase);
 
             // Add the suggestion to the database
-            
-;           suggestionDatabase.add(newSuggestion);
+            suggestionDatabase.add(newSuggestion);
             suggestionDatabase.save(); // Update the suggestions in the database
             return true;
         }
         return false;
     }
 
-    // Delete suggestion - you can add a delete method if needed
+    public boolean deleteSuggestion(Suggestion suggestion) throws IOException, ClassNotFoundException {
+        if (suggestion != null){
+            suggestionDatabase.remove(suggestion);
+            return true;
+        }
+        return false;
+    }
 
-    // Other methods...
+    public boolean editSuggestion(Camp camp, Suggestion suggestion, User user) throws IOException, ClassNotFoundException{
 
-  
+
+            suggestionDatabase.findByID(suggestion.getSuggestionID());
+        return false;
+    }
+
+    public boolean submitSuggestion() throws IOException, ClassNotFoundException{
+
+        return false;
+    }
+
+    public boolean approveChange()throws IOException, ClassNotFoundException {
+
+        return false;
+    }
+
+    public boolean viewSuggestionByID(int suggestionID){
+
+        return false;
+    }
+
 }
+
+
+/*
+ A staff can view and approve suggestions to changes to camp details from camp
+committee
+A camp committee member can submit suggestions for changes to camp details to staff
+A camp committee member can view, edit, and delete the details of his/her
+suggestions before being processed
+ */
