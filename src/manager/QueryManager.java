74d9aad7c2c38dc76744efd2d951ecc11d3f4fa0
Bateauxplanze <82026272+Paraworks@users.noreply.github.com>
2023-11-18 19:18:46 +0800

    Draft implementation

--- src/manager/QueryManager.java
+++ src/manager/QueryManager.java
@@ -1,93 +1,61 @@
 package manager;
 
+import database.QueryDatabase;
+import model.Query;
 import java.io.IOException;
 
-import database.SuggestionDatabase;
-import model.Query;
-import model.Suggestion;
-import model.User;
-import model.Camp;
+public class QueryManager {
+    private QueryDatabase queryDatabase;
 
-public class QueryManager extends SuggestionDatabase {
-    private SuggestionDatabase queryDatabase;
- 
-    // linking with suggestion database 
-    public QueryManager() throws ClassNotFoundException, IOException {
-        queryDatabase = new database.SuggestionDatabase();
-        
+    public QueryManager() {
+        this.queryDatabase = new QueryDatabase();
     }
 
-     public boolean addQuery(Camp camp, Query query) throws IOException, ClassNotFoundException {
-        if (query != null) {
-
-            // Create a new query
-            Query newQuery = new Query(query.getUserId(), camp.getId(), query.getDescription(), queryDatabase);
-
-            // Add the query to the database
-            // queryDatabase.add(newQuery);
-            queryDatabase.save(); // Update the queries in the database
-            return true;
-        }
-        return false;
+    public Query createQuery(int userId, int campId, String description) throws IOException, ClassNotFoundException {
+        int newQueryId = queryDatabase.generateNewId(); // 生成新的ID
+        Query query = new Query(userId, campId, newQueryId, description); // 使用新ID创建Query对象
+        queryDatabase.add(query); // 添加到数据库
+        return query;
     }
 
-    // delete by queryID
-    public boolean deleteByQueryID(Query query) throws IOException, ClassNotFoundException {
-        if (query != null){
-            int i = 0;
-            while (i < queryDatabase.getAll().size()) {
-                if (query.getId() == queryDatabase.getAll().get(i).getSuggestionID()) {
-                    queryDatabase.getAll().remove(i);
-                    return true;
-                }
-                i++;
+    public boolean deleteQuery(int queryID) throws IOException, ClassNotFoundException {
+        for (Query query : queryDatabase.getAll()) {
+            if (query.getId() == queryID) {
+                queryDatabase.remove(query);
+                return true;
             }
         }
         return false;
     }
 
-    // delete by campID
-    public boolean deleteByCampID(Camp camp) throws IOException, ClassNotFoundException {
-        if (camp != null){
-            queryDatabase.remove(queryDatabase.findByID(camp.getId()));
+    public boolean editQuery(int queryID, String newDescription) throws IOException, ClassNotFoundException {
+        for (Query query : queryDatabase.getAll()) {
+            if (query.getId() == queryID) {
+                query.setDescription(newDescription);
+                queryDatabase.save();
+                return true;
+            }
         }
         return false;
     }
 
-    public boolean editQuery(Camp camp, User user, Query query, String editedQuery) throws ClassNotFoundException, IOException {
+    public Query getQueryByID(int queryID) throws IOException, ClassNotFoundException {
+        for (Query query : queryDatabase.getAll()) {
+            if (query.getId() == queryID) {
+                return query;
+            }
+        }
+        return null;
+    }
 
-    int i = 0;
-    	while (i < queryDatabase.getAll().size()) {
-    		if (query.getId() == queryDatabase.getAll().get(i).getId()) {
-    			// Query newQuery = new Query(user.getId(), camp.getId(), editedQuery, queryDatabase);
-    			// queryDatabase.getAll().set(i, newQuery);
-    			return true; //returns true if successful
-    		}
-    		i++;
-    	}
-    	
+    public boolean replyToQuery(int queryID, String response) throws IOException, ClassNotFoundException {
+        for (Query query : queryDatabase.getAll()) {
+            if (query.getId() == queryID) {
+                query.setReply(response);
+                queryDatabase.save();
+                return true;
+            }
+        }
         return false;
+    }
 }
-
-public boolean viewQueryByID(int queryId) throws IOException, ClassNotFoundException{
-    if (queryDatabase.findByID(queryId) != null)
-        System.out.println(queryDatabase.findByID(queryId).getDescription());
-    return false;
-}
-
-public boolean replyQueryByID(int queryId) throws IOException, ClassNotFoundException{
-    //
-    return false;
-}
-
-    
-
-
-}
-/*
- A staff can view and reply to enquiries from students to the camp(s) his/her has created.
-A student can submit enquiries regarding a camp.
-    o Only staff and camp committees in charge of that camp can view it.
-A student can view, edit, and delete their enquiries before it is processed.
-A camp committee member can view and reply to enquiries from students to the camp they oversee.
- */
