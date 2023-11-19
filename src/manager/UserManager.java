8fe53265a3b7d9c5dbd18cfdcc9cffc0ba32001e
Bateauxplanze <82026272+Paraworks@users.noreply.github.com>
2023-11-19 20:09:14 +0800

    Merge version

--- src/manager/UserManager.java
+++ src/manager/UserManager.java
@@ -1,8 +1,9 @@
 package manager;
 
+import java.io.IOException;
+
 import database.StaffDatabase;
 import database.StudentDatabase;
-import model.Faculty;
 import model.Staff;
 import model.Student;
 import model.User;
@@ -12,8 +13,16 @@ public class UserManager {
     private StudentDatabase studentDatabase;
     private StaffDatabase staffDatabase;
     
-	
-    public UserManager(){
+	//
+    public UserManager() throws ClassNotFoundException, IOException{
+    	studentDatabase = new StudentDatabase();
+    	staffDatabase = new StaffDatabase();
+    }
+    
+    //
+    public UserManager(StudentDatabase curStudentDatabase, StaffDatabase curStaffdatabase){
+    	studentDatabase = curStudentDatabase;
+    	staffDatabase = curStaffdatabase;
     }
 
     public boolean addUser(User user) {
@@ -34,9 +43,66 @@ public class UserManager {
         return false;
     }
     
-   
-	// TODO
-    public User getUserByID(String userID) {
-    	return new Student(0, "iwi", Faculty.SCSE, "uwu", 2);
+    public boolean removeUser(User user) {
+    	try {
+    		switch(user.getType()) {
+			case STAFF:
+				staffDatabase.remove((Staff) user);
+				return true;
+			case STUDENT:
+				studentDatabase.remove((Student) user);
+				return true;
+			default:
+				throw new IllegalArgumentException(user.getType().toString());
+    		}
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+        return false;
     }
+    
+    public boolean editUser(User newUser, User currentUser) {
+    	try {
+    		switch(currentUser.getType()) {
+			case STAFF:
+				if (newUser.getType() == currentUser.getType()) {
+					staffDatabase.update((Staff)currentUser, (Staff)newUser);
+					return true;
+				}
+			case STUDENT:
+				if(newUser.getType() == currentUser.getType()) {
+					studentDatabase.update((Student)currentUser,(Student) newUser);;
+					return true;
+				}
+			default:
+				throw new IllegalArgumentException(currentUser.getType().toString());
+    		}
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+        return false;
+    }
+    
+    public User getUserByID(String userID) throws ClassNotFoundException, IOException {
+    	
+    	int i = 0;
+    	
+    	while (i < studentDatabase.getAll().size()) {
+    		if (userID == studentDatabase.getAll().get(i).getUserId()) {
+    			return studentDatabase.getAll().get(i);
+    		}
+    	}
+    	
+    	i = 0;
+    	
+    	while (i < staffDatabase.getAll().size()) {
+    		if (userID == staffDatabase.getAll().get(i).getUserId()) {
+    			return staffDatabase.getAll().get(i);
+    		}
+    	}
+    	
+    	return null;
+
+    }
+    
 }
