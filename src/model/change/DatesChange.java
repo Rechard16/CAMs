c59481d2a5da66b83919b719742f3a0e104caadb
Bateauxplanze <82026272+Paraworks@users.noreply.github.com>
2023-11-24 12:05:06 +0800

    Fix errors

--- src/model/change/DatesChange.java
+++ src/model/change/DatesChange.java
@@ -10,25 +10,20 @@ import model.Change;
 
 public class DatesChange implements Change {
     private Date newStartDate;
-    private Date newEndDate;
 
-    private DatesChange(Date newStartDate, Date newEndDate) {
+    private DatesChange(Date newStartDate) {
         this.newStartDate = newStartDate;
-        this.newEndDate = newEndDate;
     }
 
     public static DatesChange create(Context context) {
         context.print("Enter the new start date for your camp:");
         Date newStartDate = new DateReader(context).readDate();
 
-        context.print("Enter the new end date for your camp:");
-        Date newEndDate = new DateReader(context).readDate();
-
-        return new DatesChange(newStartDate, newEndDate);
+        return new DatesChange(newStartDate);
     }
 
     @Override
     public void modify(CampInfo campInfo) {
-        campInfo.setDates(List.of(newStartDate, newEndDate));
+        campInfo.setDates(List.of(newStartDate));
     }
 }
